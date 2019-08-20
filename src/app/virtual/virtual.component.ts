1c0c76b2f6ad2d7af08323260b28ead8d50cd485
Andres Alba <bryanalba92@gmail.com>
2019-08-20 17:43:37 -0500

    Mover inicio, final, mirad el ViewPort

--- src/app/virtual/virtual.component.ts
+++ src/app/virtual/virtual.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 
 
 @Component({
@@ -7,11 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./virtual.component.css']
 })
 export class VirtualComponent implements OnInit {
+
+ @ViewChild( CdkVirtualScrollViewport ) viewport: CdkVirtualScrollViewport;
+
   personas = Array(1000).fill(0);
   constructor() { }
 
   ngOnInit() {
     console.log( this.personas );
   }
+
+  public irAlFinal() {
+
+    this.viewport.scrollToIndex( this.personas.length );
+
+  }
+
+  public irAlInicio() {
+
+    this.viewport.scrollToIndex( 0 );
+
+  }
+
+  public irMitad() {
+
+    this.viewport.scrollToIndex( this.personas.length / 2 );
+
+  }
+
+  
 
 }
