8657ce4e290fd2584efc65cddb7bd1b4ac30ef25
Andres Alba <bryanalba92@gmail.com>
2019-08-20 18:02:11 -0500

    Creación de drag básico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { ScrollingModule } from '@angular/cdk/scrolling';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 
 import { AppComponent } from './app.component';
 import { VirtualComponent } from './virtual/virtual.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import 'hammerjs';
+import { DragComponent } from './drag/drag.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    VirtualComponent
+    VirtualComponent,
+    DragComponent
   ],
   imports: [
     BrowserModule,
-    ScrollingModule
+    ScrollingModule,
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
